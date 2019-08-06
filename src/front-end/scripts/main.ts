656320313149e3a3f3a17428ab4fc78cdfbbb88c
Damian <dsamsoniuk@protonmail.com>
2019-08-06 11:06:28 +0200

    rozszerzenie Promise

--- src/front-end/scripts/main.ts
+++ src/front-end/scripts/main.ts
@@ -5,8 +5,10 @@ import { Products } from './services/products.service';
 import { ProductComponent } from './components/product.component';
 
 async function main() {
+
     const $target = document.querySelector<HTMLDivElement>('.products-container');
     const products = await Products.getProducts();
+
     products.forEach((product) => {
         const p = new ProductComponent(product);
         p.render($target);
@@ -14,4 +16,18 @@ async function main() {
 
 }
 
-main();
+main()
+    .then(() => {
+        //success
+        console.log("Jest git");
+    })
+    .catch(() => {
+        //error
+        console.log("Jakis blad w catch jestem");
+        
+    })
+    .finally(() => {
+        console.log('aplikacja jedzie');
+        
+    })
+    ;
